9cd656d7e43dd59c7110b2e63afc910f1e845123
Juan Jose Curutchet <juanjo.curutchet78@gmail.com>
2023-05-22 19:41:14 -0300

    Agrego comentarios

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const personajes = document.getElementById('data-container')
 
 fetch(`https://gateway.marvel.com:443/v1/public/characters?
 ts=1&apikey=7c3793f00a63abf77335e2dd39427d9c&hash=46ab9fa0d9448707a06e98b672ea3587`)
-    // Exito
-    .then(response => response.json())  // convertir a json
+    
+    .then(response => response.json())  // convierto a json...
     .then((json) => {
         for (let hero of json.data.results) {
             const card = document.createElement("div");
@@ -12,7 +12,7 @@ ts=1&apikey=7c3793f00a63abf77335e2dd39427d9c&hash=46ab9fa0d9448707a06e98b672ea35
         <h2>${hero.name}</h2>`;
             personajes.appendChild(card);
         }
-    })    //imprimir los datos en la consola
-    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+    })  // recorro el arreglo y muestro en pantalla... 
+    .catch(err => console.log('Solicitud fallida', err)); // Capturo errores...
 
 
